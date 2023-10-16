84c8ea578ad65c68f9e12bddc2835abb18f7d9d7
Antonio Anderson <hiroto.anderson@gmail.com>
2023-10-16 16:00:52 -0300

    add fnc to checkbox and save

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import { Todo } from '../context/TodoContext'
+import React, { useState } from 'react'
+import { Todo, useTodo } from '../context/TodoContext'
 
 interface TodoItemProps {
   todo: Todo
 }
 
 const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
+  const [isEditing, setIsEditing] = useState(false)
+  const [editedTitle, setEditedTitle] = useState(todo.title)
+  const { updateTodo, removeTodo } = useTodo()
+
+  const handleSaveEdit = () => {
+    updateTodo(todo.id, { ...todo, title: editedTitle })
+    setIsEditing(false)
+  }
+
+  const handleCheckboxChange = () => updateTodo(todo.id, { ...todo, completed: !todo.completed })
+
   return (
-    <li>
-      <input type="checkbox" checked={todo.completed} />
-      <span>{todo.title}</span>
+    <li className="flex items-center justify-between py-2 border-b border-gray-300">
+      <div className="flex items-center space-x-4">
+        <input type="checkbox" checked={todo.completed} onChange={handleCheckboxChange} className="h-6 w-6 text-indigo-500" />
+        {isEditing ? (
+          <div className="flex items-center space-x-2">
+            <input type="text" value={editedTitle} onChange={(e) => setEditedTitle(e.target.value)} className="border border-gray-300 rounded px-2 py-1" />
+            <button onClick={handleSaveEdit} className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600">
+              Save
+            </button>
+          </div>
+        ) : (
+          <span className={`text-lg ${todo.completed ? 'line-through' : ''}`}>{todo.title}</span>
+        )}
+      </div>
+      <div className="space-x-2">
+        <button onClick={() => setIsEditing(!isEditing)} className="text-indigo-500 hover:underline">
+          Edit
+        </button>
+        <button onClick={() => removeTodo(todo.id)} className="text-red-500 hover:underline">
+          Delete
+        </button>
+      </div>
     </li>
   )
 }
