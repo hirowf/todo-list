85bff88b572ff0294c3575c4d3c24e5fe7516c77
Antonio Anderson <hiroto.anderson@gmail.com>
2023-10-16 14:34:19 -0300

    add `TodoItem.tsx`

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,19 @@
+import { useTodo } from '../context/TodoContext'
 import TodoForm from './TodoForm'
+import TodoItem from './TodoItem'
 
 const TodoList = () => {
-  return <TodoForm />
+  const { todos } = useTodo()
+  return (
+    <div>
+      <TodoForm />
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default TodoList
